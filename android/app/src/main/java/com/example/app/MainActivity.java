13392c2e9fd5b9d796418756ae0f7c2a9a36b5a2
Wissam_Najjom <wissam.n.najjom@gmail.com>
2026-04-30 17:39:32 +0300

    merge

--- android/app/src/main/java/com/example/app/MainActivity.java
+++ android/app/src/main/java/com/example/app/MainActivity.java
@@ -1,5 +1,10 @@
 package com.example.app;
 
+import android.webkit.PermissionRequest;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  public void onPermissionRequest(PermissionRequest request) {
+    request.grant(request.getResources());
+  }
+}
